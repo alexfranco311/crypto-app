b1fb56846e879fe7db59335c562b752f122d5bf0
alexfranco311 <alexfranco311@gmail.com>
2021-12-09 00:06:03 -0500

    deploy my react app to gh pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Axios from "axios";
+import { useEffect, useState } from "react";
+ 
 function App() {
+  const [search, setSearch] = useState("");
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    Axios.get(
+      `https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=USD`
+    ).then((res) => {
+      setCrypto(res.data.coins);
+    });
+  }, []);
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Top 100 Cryptocurrencies</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <table>
+        <thead>
+          <tr>
+            <td>Rank</td>
+            <td>Name</td>
+            <td>Symbol</td>
+            <td>Market Cap</td>
+            <td>Price</td>
+            <td>Available Supply</td>
+            <td>Volume(24hrs)</td>
+          </tr>
+        </thead>
+        {/* Mapping all the cryptos */}
+        <tbody>
+          {/* Filtering to check for the searched crypto */}
+          {crypto
+            .filter((val) => {
+              return val.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((val, id) => {
+              return (
+                <>
+                  <tr id={id}>
+                    <td className="rank">{val.rank}</td>
+                    <td className="logo">
+                      <a href={val.websiteUrl}>
+                        <img src={val.icon} alt="logo" width="30px" />
+                      </a>
+                       
+<p>{val.name}</p>
+ 
+                    </td>
+                    <td className="symbol">{val.symbol}</td>
+                    <td>$ {val.marketCap}</td>
+                    <td>$ {val.price.toFixed(2)}</td>
+                    <td>{val.availableSupply}</td>
+                    <td>{val.volume.toFixed(0)}</td>
+                  </tr>
+                </>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
+ 
 export default App;
